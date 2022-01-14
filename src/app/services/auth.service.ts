0c96dbf494da9196127e48e1174b0abdb3499ddb
ReQezeR <44375216+ReQezeR@users.noreply.github.com>
2022-01-14 23:23:02 +0100

    add auth service

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { GoogleLoginProvider, SocialAuthService, SocialUser } from 'angularx-social-login';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class AuthService {
+  private userSubject: BehaviorSubject<SocialUser|null>;
+  public userObservable: Observable<SocialUser|null>;
+
+  constructor(private socialAuthService: SocialAuthService) {
+    this.userSubject = new BehaviorSubject<SocialUser|null>(null);
+    this.userObservable = this.userSubject.asObservable();
+  }
+
+  login() {
+    this.socialAuthService.signIn(GoogleLoginProvider.PROVIDER_ID).then(
+      (data)=>{
+        this.setUserData(data);
+      },
+      (error)=>{
+        console.error(error);
+      }  
+    )
+  }
+
+  logout() {
+    this.socialAuthService.signOut().then(
+      ()=>{
+        localStorage.removeItem('user');
+        this.userSubject.next(null);
+      }
+    );
+  }
+
+  getUserData(){
+    return this.socialAuthService.authState;
+  }
+
+  setUserData(user: SocialUser){
+    localStorage.setItem('user', JSON.stringify(user));
+    this.userSubject.next(user);
+  }
+}
